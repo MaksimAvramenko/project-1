26660e96c2341520c5a8ba763f7936623a58414c
Maksim <m.avramenko11@gmail.com>
2021-12-23 15:37:13 +0300

    added schema and data

---
+++ data.sql
@@ -0,0 +1,16 @@
+insert into usr (first_name, middle_name, last_name, email) values
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com');
+
+insert into usr (first_name, middle_name, last_name, email) values
+('Максим', 'Алексеевич', 'Лебедев', 'lebedev@my.example.com'),
+('Савелий', 'Даниилович', 'Васильев', 'vasilev@my.example.com'),
+('Богдан', 'Егорович', 'Филимонов', 'filimonov@my.example.com'),
+('Александр', 'Кириллович', 'Грачев', 'grachev@my.example.com'),
+('Ярослав', 'Никитич', 'Борисов', 'borisov@my.example.com'),
+('Кирилл', 'Михайлович', 'Корнев', 'kornev@my.example.com'),
+('Кирилл', 'Михайлович', 'Кузнецов', 'kuznetsov@my.example.com'),
+('Александр', 'Семёнович', 'Мальцев', 'maltzhev@my.example.com'),
+('Артём', 'Ильич', 'Козлов', 'kozlov@my.example.com'),
+('Дмитрий', 'Павлович', 'Голубев', 'golubev@my.example.com');
